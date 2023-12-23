2b4a56fa0040c2de95424d6533bc74bea045fe18
unitythecoder <unitythecodergit@gmail.com>
2023-12-23 03:00:12 +0100

    [+Feat] { Titan supporting machine code writing }

---
+++ cenum.py
@@ -0,0 +1,6 @@
+class vEnum():
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return f'{cls.__name__}.{self.value}'
---
+++ titan.py
@@ -0,0 +1,44 @@
+from opcodes import *
+from dtype import *
+from codewriter import *
+from executable import *
+
+
+m_prog = Executable()
+
+
+m_prog.new_constant(
+    "magic",
+    0x1AF
+)
+
+m_prog.new_variable(
+    "offset",
+    None,
+    Types.INT32
+)
+
+m_prog.new_function(
+    "add",
+    [("a", Types.INT32)],
+    [
+        m_prog.Move(Registers.RDX, 50),
+        m_prog.Move(Registers.RCX, 5),
+        m_prog.Add(Registers.RAX, Registers.RCX),
+        m_prog.Return()
+    ]
+)
+
+m_prog.new_function(
+    "main",
+    [],
+    [
+        m_prog.Push(1),
+        m_prog.Push(2),
+        m_prog.Call("add"),
+    ]
+)
+
+
+
+mwt = mcWriter(m_prog, "gen_mbin")
